9e993b5685c60e7ce86854ff3ea0fca0d64005e3
João Denadai <joaodenadai86@outlook.com>
2025-11-17 17:49:31 -0300

    fix: installed missing tailwind/postcss dependencies and cleaned config structure

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,8 @@
 // postcss.config.js
 module.exports = {
   plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
+    // Esta é a sintaxe padrao que deve funcionar
+    'tailwindcss': {}, 
+    'autoprefixer': {},
   },
 };
